85c6ff1d3794083cea9844704c4ba5d5018f5081
diptanshumkm <diptanshu.uae@gmail.com>
2023-11-15 12:48:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [formData, setFormData] = useState( {firstName: "", lastName: "", email: "", country: "", street: "", city: "", state: "", ZIP: "", comments: false, candidates: "", offers: ""} )
+
+  function changeHandler(event){
+
+    setFormData( prevData => {
+      return{
+        ...prevData,
+        [event.target.name] : event.target.type === "checkbox" ? event.target.checked : event.target.value
+      }
+    }
+    )
+  }
+
+  // console.log(formData)
+
+  function submitHandler(event){
+    event.preventDefault()
+    console.log("printing........")
+    console.log(formData);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+
+      <div>
+        <h1>Forms</h1>
+      </div>
+
+      {/* Entire form container */}
+      <form onSubmit={submitHandler}>
+
+        <label htmlFor='firstName'>First Name</label>
+        <input 
+          name='firstName'
+          onChange={changeHandler}
+          value={formData.firstName}
+        ></input>
+
+        <br></br>
+        <br></br>
+
+        <label htmlFor='lastName'>Last Name</label>
+        <input 
+          name='lastName'
+          onChange={changeHandler}
+          value={formData.lastName}
+        ></input>
+
+        <br></br>
+        <br></br>
+
+        <label htmlFor='email'>Email</label>
+        <input 
+          name='email'
+          onChange={changeHandler}
+          value={formData.email}
+        ></input>
+
+        <br></br>
+        <br></br>
+
+        <label htmlFor='country'> Country </label>
+        <select
+          value={formData.country}
+          name='country'
+          id='country'
+          onChange={changeHandler}
         >
-          Learn React
-        </a>
-      </header>
+
+          <option value="NewZealand">NewZealand</option>
+          <option value="Australia">Australia</option>
+          <option value="Germany">Germany</option>
+          <option value="Ireland">Ireland</option>
+
+        </select>
+
+
+        
+        <br></br>
+        <br></br>
+
+        <label htmlFor='street'>Street</label>
+        <input 
+          name='street'
+          onChange={changeHandler}
+          value={formData.street}
+        ></input>
+
+
+        
+        <br></br>
+        <br></br>
+
+        <label htmlFor='city'>City</label>
+        <input 
+          name='city'
+          onChange={changeHandler}
+          value={formData.city}
+        ></input>
+
+        
+        <br></br>
+        <br></br>
+
+        <label htmlFor='state'>State</label>
+        <input 
+          name='state'
+          onChange={changeHandler}
+          value={formData.state}
+        ></input>
+
+        
+        <br></br>
+        <br></br>
+
+        <label htmlFor='lastName'>Zip / Postal</label>
+        <input 
+          name='ZIP'
+          onChange={changeHandler}
+          value={formData.ZIP}
+        ></input>
+
+        <div>
+          <h4>By Email</h4>
+        </div>
+
+        <br></br>
+
+        <input
+          type='checkbox'
+          checked = {formData.comments}
+          onChange={changeHandler}
+          id='comments'
+          name='comments'
+        ></input>
+        <label htmlFor='comments'>Comments</label>
+
+
+        <br></br>
+        <br></br>
+
+        
+        <input
+          type='checkbox'
+          checked = {formData.candidates}
+          onChange={changeHandler}
+          id='candidates'
+          name='candidates'
+        ></input>
+        <label htmlFor='candidates'>Candidates</label>
+
+        <br></br>
+        <br></br>
+        
+        <input
+          type='checkbox'
+          checked = {formData.offers}
+          onChange={changeHandler}
+          id='offers'
+          name='offers'
+        ></input>
+        <label htmlFor='offers'>Offers</label>
+
+        <div>
+          <h4>Push Notification</h4>
+        </div>
+
+        <button>Submit</button>
+
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
